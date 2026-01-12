47350818722408f2f54eefe6311da226c585b0b5
Al-fineway <sommydalu63@gmail.com>
2026-01-12 15:26:04 +0100

    Update UI

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/quick-ride-app/',   // 👈 EXACT repo name
 })
